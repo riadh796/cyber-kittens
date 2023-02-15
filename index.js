39d11ce5e9340ad3061fcc6ef0923d33123df235
unknown <ainouzusa@gmail.com>
2023-02-16 00:21:54 +0100

    3 failed, 5 skipped, 6 passed

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const { User } = require('./db');
+const { User,Kitten } = require('./db');
 
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
@@ -21,6 +24,70 @@ app.get('/', async (req, res, next) => {
 
 // Verifies token with jwt.verify and sets req.user
 // TODO - Create authentication middleware
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(401);
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+// GET /kittens/:id
+app.get('/kittens/:id', authenticateJWT, async (req, res) => {
+  const kitten = await Kitten.findByPk(req.params.id);
+
+  if (!kitten) {
+    res.sendStatus(401);
+  } else if (kitten.ownerId !== req.user.id) {
+    res.sendStatus(401);
+  } else {
+    res.json(kitten);
+  }
+});
+// POST /kittens
+app.post('/kittens', authenticateJWT, async (req, res) => {
+  if (!req.user) {
+    res.sendStatus(401);
+  } else {
+    const kitten = await Kitten.create({
+      name: req.body.name,
+      age: req.body.age,
+      color: req.body.color,
+      ownerId: req.user.id
+    });
+
+    res.status(201).json({
+      name: kitten.name,
+      age: kitten.age,
+      color: kitten.color
+    });
+  }
+});
+
+// DELETE /kittens/:id
+app.delete('/kittens/:id', authenticateJWT, async (req, res) => {
+  const kitten = await Kitten.findByPk(req.params.id);
+
+  if (!kitten) {
+    res.sendStatus(401);
+  } else if (kitten.ownerId !== req.user.id) {
+    res.sendStatus(401);
+  } else {
+    await kitten.destroy();
+    res.sendStatus(204);
+  }
+});
 
 // POST /register
 // OPTIONAL - takes req.body of {username, password} and creates a new user with the hashed password
